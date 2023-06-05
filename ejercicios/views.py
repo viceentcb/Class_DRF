b191e0efb2d5a95dad115e0876c49a8aa5620d19
Vicent <viceentcb@gmail.com>
2023-06-05 18:13:10 +0200

    feature: ejercicios

--- ejercicios/views.py
+++ ejercicios/views.py
@@ -1,5 +1,6 @@
 # Django and DRF imports
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 # proof class imports
 from ejercicios.models import Television
@@ -12,7 +13,7 @@ class TelevisionViewSet(ModelViewSet):
     lookup_field = 'numero_de_serie'
 
 
-class NeveraViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Update, GenericViewSet):
+class NeveraViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.UpdateModelMixin, GenericViewSet):
     queryset = Television.objects.all()
     serializer_class = TelevisionSerializer
     lookup_field = 'id'
