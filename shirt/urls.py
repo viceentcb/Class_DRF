7094e4276fb387a5c95f3f666a582276333fe132
Vicent <viceentcb@gmail.com>
2023-06-19 17:39:37 +0200

    feature: shirts me

--- shirt/urls.py
+++ shirt/urls.py
@@ -2,10 +2,11 @@
 from rest_framework.routers import SimpleRouter
 
 # proof class import
-from .views import ShirtViewSet
+from .views import ShirtViewSet, MeShirtView
 
 router = SimpleRouter()
 
 router.register(r'shirts', ShirtViewSet, basename="shirts")
+router.register(r'me/shirts', MeShirtView, basename="me_shirts")
 
 urlpatterns = router.urls
--- shirt/views.py
+++ shirt/views.py
@@ -23,12 +23,6 @@ class ShirtViewSet(mixins.CreateModelMixin,
     serializer_class = ListShirtSerializer
     lookup_field = "id"
 
-    def get_queryset(self):
-        queryset = self.queryset
-        if self.action in ("me_list", "me"):
-            queryset = queryset.filter(user_id=self.request.user.id)
-        return queryset
-
     def create(self, request, *args, **kwargs):
         request.data['user_id'] = request.user.id
         serializer = self.get_serializer(data=request.data)
@@ -37,40 +31,23 @@ class ShirtViewSet(mixins.CreateModelMixin,
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    @action(detail=True, methods=['PUT', 'PATCH', 'DELETE'])
-    def me(self, request, id):
-
-        if request.method in ('PUT', 'PATCH'):
-            instance = self.get_object()
-            serializer = self.get_serializer(instance, data=request.data, partial=True)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data)
-
-        elif request.method == 'DELETE':
-            instance = self.get_object()
-            instance.delete()
-
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-    @action(detail=False, methods=['GET'], url_path="me-list", url_name="me-list")
-    def me_list(self, request):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
     def get_serializer_class(self):
         if self.action == "create":
             return CreateShirtSerializer
-        elif self.action == 'me':
-            return UpdateShirtSerializer
-        elif self.action == 'me_list':
-            return ListProfileShirtSerializer
+        return self.serializer_class
 
+
+class MeShirtView(mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin, GenericViewSet):
+
+    queryset = Shirt.objects.all()
+    serializer_class = ListProfileShirtSerializer
+    lookup_field = "id"
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user_id=self.request.user.id)
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action in ("update", "partial_update"):
+            return UpdateShirtSerializer
         return self.serializer_class
